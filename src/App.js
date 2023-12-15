8336cf23e2d4de17691c1c7e73896b8fa596fc19
Scott Adams <scott_adams080291@hotmail.com>
2023-12-15 20:28:37 +0000

    feat(initial): click to complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript";
 
-function App() {
+function CodeEditor() {
+  const [code, setCode] = useState(
+    "// Click on the elements above to fill gaps\n___gap___\n___gap___"
+  );
+
+  const insertCode = (snippet) => {
+    setCode((currentCode) => currentCode.replace("___gap___", snippet));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        className="clickable"
+        onClick={() => insertCode("console.log('Hello');")}
+      >
+        console.log('Hello');
+      </div>
+      <div className="clickable" onClick={() => insertCode("let x = 10;")}>
+        let x = 10;
+      </div>
+
+      <ControlledEditor
+        value={code}
+        options={{
+          mode: "javascript",
+          lineNumbers: true,
+        }}
+        onBeforeChange={(editor, data, value) => setCode(value)}
+      />
+      <div id="code">{code}</div>
     </div>
   );
 }
 
-export default App;
+export default CodeEditor;
